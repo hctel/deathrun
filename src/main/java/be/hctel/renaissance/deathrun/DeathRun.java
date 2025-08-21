7fa05cab3094f2cc9615c8a1bca04d9dedadbf13
Hugo de Hepcée <22167@ecam.be>
2025-08-21 16:57:58 +0200

    Added Multiverse Core integration

--- src/main/java/be/hctel/renaissance/deathrun/DeathRun.java
+++ src/main/java/be/hctel/renaissance/deathrun/DeathRun.java
@@ -1,22 +1,42 @@
 package be.hctel.renaissance.deathrun;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mvplugins.multiverse.core.MultiverseCore;
+import org.mvplugins.multiverse.core.MultiverseCoreApi;
+import org.mvplugins.multiverse.core.world.WorldManager;
 
+import be.hctel.renaissance.deathrun.commands.StaffCommands;
 import be.hctel.renaissance.deathrun.commands.TestTrapCommand;
 import be.hctel.renaissance.deathrun.listeners.BlockListeners;
 import be.hctel.renaissance.deathrun.listeners.PlayerListener;
+import be.hctel.renaissance.global.mapmanager.MapManager;
 
 public class DeathRun extends JavaPlugin {
 	
-	public static String header = "§8▍ §§4Death§cRun§8 ▏ ";
+	public String header = "§8▍ §§4Death§cRun§8 ▏ ";
 	
-	public static Plugin plugin;
+	public Plugin plugin;
+	
+	public MultiverseCore core;
+	public MultiverseCoreApi mvAPI;
+	public WorldManager worldManager;
+	
+	public MapManager mapManager; 
 
 	@Override
 	public void onEnable() {
 		getLogger().info("Enabling DeathRun");
+		
 		plugin = this;
+		
+		core = (MultiverseCore) Bukkit.getServer().getPluginManager().getPlugin("Multiverse-Core");
+		mvAPI = core.getApi();
+		worldManager = mvAPI.getWorldManager();
+		
+		mapManager = new MapManager(this);
+		
 		loadCommands();
 		registerListeners();
 		getLogger().info("Enabled DeathRun");
@@ -28,7 +48,8 @@ public class DeathRun extends JavaPlugin {
 	}
 	
 	private void loadCommands() {
-		getCommand("testtrap").setExecutor(new TestTrapCommand());
+		getCommand("testtrap").setExecutor(new TestTrapCommand(this));
+		getCommand("gm").setExecutor(new StaffCommands(this));
 	}
 	
 	private void registerListeners() {
