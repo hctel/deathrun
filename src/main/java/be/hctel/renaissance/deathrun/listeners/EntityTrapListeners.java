3fc1d4190043b6f1a83816127c1879b55454aea6
Hugo de Hepcée <22167@ecam.be>
2025-10-05 17:19:37 +0200

    Forgot to implement listener

--- src/main/java/be/hctel/renaissance/deathrun/listeners/EntityTrapListeners.java
+++ src/main/java/be/hctel/renaissance/deathrun/listeners/EntityTrapListeners.java
@@ -3,9 +3,10 @@ package be.hctel.renaissance.deathrun.listeners;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Arrow;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
 
-public class EntityTrapListeners {
+public class EntityTrapListeners implements Listener {
 	@EventHandler
 	public void onHit(ProjectileHitEvent e) {
 		if(e.getEntity() instanceof Arrow && e.getEntity().getShooter() instanceof ArmorStand) {
